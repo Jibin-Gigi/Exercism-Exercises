8d1bbc65037e280f7f5dea9aea4e7e7939758310
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-06 23:55:22 +0530

    Update armstrong_numbers.py

--- armstrong_numbers.py
+++ armstrong_numbers.py
@@ -2,6 +2,6 @@ def is_armstrong_number(number):
     temp=str(number)
     sum=0
     length=len(temp)
-    for i in range(0,length):
-        sum=int(temp[i])**length+sum
-    return sum==number
+    for i in temp:
+        sum=int(i)**length+sum
+    return sum==number 
