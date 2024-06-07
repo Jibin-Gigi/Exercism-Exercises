5b14e66ca7e8ca067fe9eaff8b7cd32fccd857af
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-08 00:29:04 +0530

    Update bob.py

--- bob.py
+++ bob.py
@@ -1,11 +1,17 @@
-def response(hey_bob):
-    hey_bob=hey_bob.strip()
-    if hey_bob.isupper() and hey_bob.endswith('?'):
+'''def response(hey_bob:str) ->str : implies that 
+Function parameters: hey_bob: str indicates that the parameter hey_bob is expected to be of type str (string).
+Return type: -> str after the function declaration indicates that the function is expected to return a string.'''
+
+def response(hey_bob:str) ->str :
+    hey_bob = hey_bob.strip()
+    if hey_bob.isupper() and hey_bob.endswith("?"):
         return "Calm down, I know what I'm doing!" 
-    if hey_bob.endswith('?'):
+    if hey_bob.endswith("?"):
         return "Sure."
     if hey_bob.isupper():
         return "Whoa, chill out!"
-    if hey_bob == "":
+        
+    #Checking hey_bob is not an empty string
+    if not hey_bob: 
         return "Fine. Be that way!"
     return "Whatever."
