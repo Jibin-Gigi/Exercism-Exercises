4a49249b0bfa1fd7016b42b8864688892351aad5
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-09 00:16:10 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -72,4 +72,13 @@ def student_ranking(student_scores, student_names):
     return rank
 
 
+def perfect_score(student_info):
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
 
+    :param student_info: list - of [<student name>, <score>] lists.
+    :return: list - first `[<student name>, 100]` or `[]` if no student score of 100 is found.
+    """
+    for student in student_info:
+        if student[1]==100 :
+            return student
+    return []
