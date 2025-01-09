4c602a900443c69204fba3dc4b198bac4484ebdd
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-01-09 23:16:17 +0530

    Update annalyns-infiltration.js

--- JavaScript/annalyns-infiltration.js
+++ JavaScript/annalyns-infiltration.js
@@ -72,9 +72,5 @@ export function canFreePrisoner(
   prisonerIsAwake,
   petDogIsPresent,
 ) {
-  if(petDogIsPresent){
-    return !archerIsAwake
-  } else {
-    return prisonerIsAwake && !knightIsAwake && !archerIsAwake
-  }
+  return !archerIsAwake && (petDogIsPresent || prisonerIsAwake && !knightIsAwake)
 }
