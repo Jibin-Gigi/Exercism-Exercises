cdcd3d0b4cf65373acd6399b1408503c27636f82
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-02-04 21:39:58 +0530

    Create triangle.js

---
+++ JavaScript/triangle.js
@@ -0,0 +1,29 @@
+//
+// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Triangle {
+  constructor(...sides) {
+    [this.a, this.b, this.c] = sides;
+  }
+  
+  isValid() {
+    return !(this.a === 0 || this.b === 0 || this.c === 0) && (
+      this.a + this.b >= this.c &&
+      this.a + this.c >= this.b &&
+      this.b + this.c >= this.a
+    );
+  }
+  get isEquilateral() {
+    return this.isValid() && (this.a === this.b && this.b === this.c);
+  }
+
+  get isIsosceles() {
+    return this.isValid() && (this.a === this.b || this.a === this.c || this.b === this.c);
+  }
+
+  get isScalene() {
+    return this.isValid() && (this.a !== this.b && this.a !== this.c && this.b !== this.c);
+  }
+}
